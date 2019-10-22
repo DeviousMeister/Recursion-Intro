6e1c256515f6f35571742a7027fd865379232ed7
Moises <mdaboin@wustl.edu>
2019-10-22 11:46:25 -0500

    finally

--- src/lab6/RecursiveMethods.java
+++ src/lab6/RecursiveMethods.java
@@ -9,7 +9,12 @@ public class RecursiveMethods {
      */
     public static String dragon(int n) {
         //FIXME Recursively compute dragon curves
-        return "";
+        if(n==0){
+            return ("F-H");
+        }
+        else{
+            return dragon(n-1).replaceAll("F", "f-h").replaceAll("H", "f+h").replaceAll("f", "F").replaceAll("h", "H");
+        }
     }
 
     /**
@@ -18,19 +23,51 @@ public class RecursiveMethods {
      * @param exp the exponent - can be positive or negative
      * @return base ^ exponent
      */
+
     public static double exponent(int base, int exp) {
         //FIXME Recursively compute base^exp
-        return 0;
+        if(base==0){
+            return 0;
+        }
+       /* if(exp==2){
+            return base*base;
+        }
+        if(exp==1){
+            return base;
+        }*/
+        if(exp==0) {
+            return 1;
+        }
+        /*else if(base<0 && (0==exp%2) && exp<0){
+            return -1/(exponent(base, exp-1));
+        }
+        else if(base<0 && (0!=exp%2) && exp<0){
+           return -1/(exponent(base, (exp-1)*-1));
+        }*/
+        else if(exp<0){
+            return 1/(double)base*(exponent(base, (exp+1)));
+        }
+        else{
+           return (double)base * exponent(base, exp-1);
+        }
     }
-
     /**
      * Recursively compute the sum of elements in an array
      * @param array an array of integers
      * @return the sum of the elements in values
      */
+    public static int sumHelper(int index, int[] array){
+        if(index<0){
+            return 0;
+        }
+        else{
+            return array[index] + sumHelper(index-1, array);
+        }
+    }
     public static int arraySum(int[] array) {
         // FIXME: Recursively compute the sum of the values in an array
-        return 0;
+        int index=array.length-1;
+        return sumHelper(index, array);
     }
 
     /**
@@ -38,8 +75,25 @@ public class RecursiveMethods {
      * @param paths an array containing paths
      * @return the length of the longest path that was found
      */
+    public static int maxHelper(int[][] paths, int row, int column){
+
+        if(row<0 || column<0 || row>paths.length-1 || column>paths[0].length-1 || paths[row][column] != 1){
+            return 0;
+        }
+        else{
+            paths[row][column]=0;
+        }
+            int north = 1+maxHelper(paths, row-1, column);
+            int south = 1+maxHelper(paths, row+1, column);
+            int east = 1+maxHelper(paths, row, column+1);
+            int west = 1+maxHelper(paths, row, column-1);
+            int max1 = Math.max(north, south);
+            int max2 = Math.max(east, west);
+            int max3 = Math.max(max1, max2);
+            return max3;
+    }
     public static int maxPathLength(int[][] paths) {
         //FIXME Find and return the length of the longest path in the array
-        return 0;
+        return maxHelper(paths, 0, 0);
     }
 }
